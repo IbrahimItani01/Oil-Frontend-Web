3721fbbf938ed7ad2a719cc95ba9f4b8300b7dd2
IbrahimItani <i.itani2002@gmail.com>
2025-03-09 02:28:07 +0200

    components<>imported the util functiion that handles login

--- components/base/AuthForm.tsx
+++ components/base/AuthForm.tsx
@@ -8,6 +8,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { handleLogin } from "@/lib/auth/handleLogin.util";
 
 export default function AuthForm() {
 	const [formData, setFormData] = useState({
@@ -25,6 +26,7 @@ export default function AuthForm() {
 	const handleSubmit = (e: React.FormEvent) => {
         // TODO: Handle form submission
 		e.preventDefault();
+		handleLogin(formData)
 		console.log(formData);
 	};
 
