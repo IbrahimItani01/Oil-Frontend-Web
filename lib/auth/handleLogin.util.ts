1e86747577f4a620eeb7503a871e418f075522bb
IbrahimItani <i.itani2002@gmail.com>
2025-03-09 02:42:11 +0200

    utils<>edited handleLogin return type to return username and jwt token. Also returned mock data to test before linking

--- lib/auth/handleLogin.util.ts
+++ lib/auth/handleLogin.util.ts
@@ -1,6 +1,12 @@
 export const handleLogin = async (formData: {
 	email: string;
 	password: string;
-}) => {
-    // TODO: fetch the login api and add return JWT Token, and username
+}): Promise<{ userName: string; jwtToken?: string }> => {
+	// TODO: Fetch the login API and return JWT token and username
+
+	// Example response (will be replaced by real data after linking)
+	return {
+		userName: formData.email,
+		jwtToken: "sample.jwt.token",
+	};
 };
