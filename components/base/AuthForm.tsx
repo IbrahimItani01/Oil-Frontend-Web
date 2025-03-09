494be5cdd78224a0363128defe2a3f328b779ee9
IbrahimItani <i.itani2002@gmail.com>
2025-03-09 02:16:59 +0200

    components<>declared AuthForm component

---
+++ components/base/AuthForm.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import type React from "react";
+
+import { useState } from "react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+
+export default function AuthForm() {
+	}
