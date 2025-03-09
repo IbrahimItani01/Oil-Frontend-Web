c9b13676da11ca47042233a3881b27954bfdcec3
IbrahimItani <i.itani2002@gmail.com>
2025-03-09 14:25:20 +0200

    layouts<>wrapped the dashboard route layout file with sidebar provider from Sidebar component from shadcn

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,10 @@
+import AppSidebar from "@/components/base/AppSidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+	return (
+		<SidebarProvider>
+			
+		</SidebarProvider>
+	);
+}
