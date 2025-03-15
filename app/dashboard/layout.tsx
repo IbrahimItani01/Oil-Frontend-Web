fd96620bd633ed141984576b8cb5f066df6d1858
IbrahimItani <i.itani2002@gmail.com>
2025-03-15 12:30:19 +0200

    layouts<>imported & intergated the ntoifications section in dashboard layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import AppSidebar from "@/components/base/AppSidebar";
 import { DashboardHeader } from "@/components/base/DashboardHeader";
+import NotificationsSection from "@/components/base/NotificationsSection";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
@@ -12,6 +13,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 
 					{children}
 				</div>
+				<NotificationsSection />
 			</div>
 		</SidebarProvider>
 	);
---
+++ lib/constants/notifications.constants.ts
@@ -0,0 +1,39 @@
+import { Notification } from "@/components/base/dashboard/notifications/NotificationCard";
+
+export const notifications: Notification[] = [
+	{
+		id: "1",
+		avatar: "/placeholder.svg?height=40&width=40",
+		name: "Natali Craig",
+		action: "booked",
+		timestamp: "Just now",
+	},
+	{
+		id: "2",
+		avatar: "/placeholder.svg?height=40&width=40",
+		name: "Kate Morrison",
+		action: "canceled",
+		timestamp: "Just now",
+	},
+	{
+		id: "3",
+		avatar: "/placeholder.svg?height=40&width=40",
+		name: "Drew Cano",
+		action: "booked",
+		timestamp: "Just now",
+	},
+	{
+		id: "4",
+		avatar: "/placeholder.svg?height=40&width=40",
+		name: "Orlando Diggs",
+		action: "canceled",
+		timestamp: "Just now",
+	},
+	{
+		id: "5",
+		avatar: "/placeholder.svg?height=40&width=40",
+		name: "Andi Lane",
+		action: "booked",
+		timestamp: "Just now",
+	},
+];
