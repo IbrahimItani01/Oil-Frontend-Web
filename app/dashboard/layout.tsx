f058967596ac7c2fae35a87abd5fc5358a1151a1
IbrahimItani <i.itani2002@gmail.com>
2025-03-15 12:26:50 +0200

    layouts<>imported the Dashboard heaer component into dashboard layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,14 +1,18 @@
 import AppSidebar from "@/components/base/AppSidebar";
+import { DashboardHeader } from "@/components/base/DashboardHeader";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
 		<SidebarProvider>
 			<AppSidebar />
-			<main>
-				<SidebarTrigger />
-				{children}
-			</main>
+			<div className='flex w-full h-[100vh]'>
+				<div className='w-full flex flex-col'>
+					<DashboardHeader />
+
+					{children}
+				</div>
+			</div>
 		</SidebarProvider>
 	);
 }
