e116b6b38e55446320d91000881621017e014c49
IbrahimItani <i.itani2002@gmail.com>
2025-03-23 14:55:27 +0200

    components<>created the logout modal and added its logic to the sidebar logic

--- components/base/sidebar/Footer.tsx
+++ components/base/sidebar/Footer.tsx
@@ -2,22 +2,46 @@
 import { useSidebar } from "@/components/ui/sidebar";
 import { LogOut } from "lucide-react";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
+import LogoutModal from "./LogoutModal";
 
 const Footer = () => {
 	const { open } = useSidebar();
 	const navigate = useRouter();
+	const [showModal, setShowModal] = useState(false);
+
 	const handleLogout = () => {
 		// handle logout
 		localStorage.removeItem("token");
 		navigate.push("/");
-
 	};
+
+	const handleOpenModal = () => {
+		setShowModal(true);
+	};
+
+	const handleCloseModal = () => {
+		setShowModal(false);
+	};
+
 	return (
-		<div onClick={handleLogout} className={`cursor-pointer flex items-center ${!open? 'justify-center':'ml-2'} gap-2 text-red-400 hover:text-red-700 transition-all`}>
-			<LogOut size={18} />
-			{open && <p>Logout</p>}
-		</div>
+		<>
+			<div
+				onClick={handleOpenModal}
+				className={`cursor-pointer flex items-center ${
+					!open ? "justify-center" : "ml-2"
+				} gap-2 text-red-400 hover:text-red-700 transition-all`}
+			>
+				<LogOut size={18} />
+				{open && <p>Logout</p>}
+			</div>
+			{showModal && (
+				<LogoutModal
+					handleCloseModal={handleCloseModal}
+					handleLogout={handleLogout}
+				/>
+			)}
+		</>
 	);
 };
 
