5607e0e6813056c4b2f06f816b845df599461232
IbrahimItani <i.itani2002@gmail.com>
2025-04-12 20:04:07 +0300

    apis<>linked user login

--- apis/auth.apis.ts
+++ apis/auth.apis.ts
@@ -1,22 +1,36 @@
-// TODO: Fetch Auth apis
+import dotenv from "dotenv";
+import axios from "axios";
+
+dotenv.config();
 
 type Login = {
 	email: string;
 	password: string;
 };
+
 type LoginReturn = {
 	userName: string;
 	jwtToken: string;
 };
+
 export const handleLogin = async ({
 	email,
 	password,
 }: Login): Promise<LoginReturn> => {
-	// TODO: Fetch the login API and return JWT token and username
+	try {
+		const response = await axios.post(`${process.env.APIS_URL}/auth/login`, {
+			email,
+			password,
+		});
 
-	// Example response (will be replaced by real data after linking)
-	return {
-		userName: email,
-		jwtToken: "sample.jwt.token",
-	};
+		const { data } = response.data;
+
+		return {
+			userName: data.user.name ?? data.user.email ?? "Unknown User",
+			jwtToken: data.access_token,
+		};
+	} catch (error: any) {
+		console.error("Login failed:", error.response?.data || error.message);
+		throw new Error("Login request failed");
+	}
 };
