09c2ebc1739961993efb6789558bba6ea02a57e5
IbrahimItani <i.itani2002@gmail.com>
2025-04-18 18:37:09 +0300

    wrapper<>modified the app warapper to handle syncing users data upon refersh or route change

--- lib/AppWrapper.tsx
+++ lib/AppWrapper.tsx
@@ -1,15 +1,20 @@
 "use client";
 import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { useAppDispatch } from "@/store/store";
-import { setUsers } from "@/store/slices/users.slice"; // 1. Import setUsers
-import { usersData } from "./content/users.content";
+import { useRouter } from "next/navigation"; // Import useRouter from next/navigation
+import { useAppDispatch, useAppSelector } from "@/store/store";
+import { setUsers } from "@/store/slices/users.slice";
 import { setEmployees } from "@/store/slices/employees.slice";
+import { usersData } from "./content/users.content";
 import { employeesData } from "./content/employees.content";
+import {
+	useSyncOnPageUnload,
+	useSyncOnRouteChange,
+} from "./handlers/usersData.handlers";
 
 const AppWrapper = ({ children }: { children: React.ReactNode }) => {
 	const dispatch = useAppDispatch();
 	const router = useRouter();
+	const modifiedUsers = useAppSelector((state) => state.users.modifiedUsers);
 
 	useEffect(() => {
 		const token = localStorage.getItem("token");
@@ -22,6 +27,9 @@ const AppWrapper = ({ children }: { children: React.ReactNode }) => {
 			router.push("/auth");
 		}
 	}, [dispatch, router]);
+
+	useSyncOnPageUnload(modifiedUsers);
+	useSyncOnRouteChange(modifiedUsers);
 
 	return <>{children}</>;
 };
