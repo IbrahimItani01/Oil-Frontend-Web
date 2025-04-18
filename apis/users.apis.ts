a9f4c039404ce02c0da2f725fe61677a39533b98
IbrahimItani <i.itani2002@gmail.com>
2025-04-18 18:14:13 +0300

    apis<>Created a function to synce modified users data with backend

--- apis/users.apis.ts
+++ apis/users.apis.ts
@@ -1,19 +1,17 @@
 import axios from "axios";
+import { User } from "@/lib/content/users.content";
 
-export const handleBlockUser = async (token: string, phone: string | null) => {
-	// try {
-	// 	await axios.post(
-	// 		`${process.env.NEXT_PUBLIC_APIS_URL}/admin/block_user`,
-	// 		{
-	// 			phone_number: phone,
-	// 		},
-	// 		{
-	// 			headers: {
-	// 				Authorization: `Bearer ${token}`,
-	// 			},
-	// 		}
-	// 	);
-	// } catch (error) {
-	// 	console.error("Error blocking user:", error);
-	// }
+export const syncModifiedUsers = async (modifiedUsers: User[]) => {
+	const syncUrl = process?.env?.NEXT_PUBLIC_USERS_SYNC_DATA_URL;
+	if (syncUrl) {
+		try {
+			await axios.post(syncUrl, modifiedUsers, {
+				headers: {
+					"Content-Type": "application/json",
+				},
+			});
+		} catch (error) {
+			console.error("Error syncing users:", error);
+		}
+	}
 };
