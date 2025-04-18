52381563d13c068a16b4ecafa2540dbc590e7d64
IbrahimItani <i.itani2002@gmail.com>
2025-04-19 01:49:01 +0300

    wrapper<>re-structured the app wrapper to handel data sycning of employees and users

--- lib/AppWrapper.tsx
+++ lib/AppWrapper.tsx
@@ -4,18 +4,22 @@ import { useRouter } from "next/navigation";
 import { useAppDispatch, useAppSelector } from "@/store/store";
 import { setUsers } from "@/store/slices/users.slice";
 import { setEmployees } from "@/store/slices/employees.slice";
-import { usersData } from "./content/users.content";
-import { employeesData } from "./content/employees.content";
+import { User, usersData } from "./content/users.content";
+import { Employee, employeesData } from "./content/employees.content";
 import {
 	useSyncOnPageUnload,
 	useSyncOnRouteChange,
 } from "./handlers/usersData.handlers";
 import { fetchUsers } from "@/apis/users.apis";
+import { fetchEmployees } from "@/apis/employees.apis";
 
 const AppWrapper = ({ children }: { children: React.ReactNode }) => {
 	const dispatch = useAppDispatch();
 	const router = useRouter();
 	const modifiedUsers = useAppSelector((state) => state.users.modifiedUsers);
+	const modifiedEmployees = useAppSelector(
+		(state) => state.employees.modifiedEmployees
+	);
 
 	useEffect(() => {
 		const token = localStorage.getItem("token");
@@ -23,9 +27,11 @@ const AppWrapper = ({ children }: { children: React.ReactNode }) => {
 		const initializeData = async () => {
 			if (token) {
 				const apiUsers = await fetchUsers();
+				const apiEmployees = await fetchEmployees();
 				dispatch(setUsers(apiUsers.length ? apiUsers : usersData));
-
-				dispatch(setEmployees(employeesData));
+				dispatch(
+					setEmployees(apiEmployees.length ? apiEmployees : employeesData)
+				);
 				router.push("/dashboard");
 			} else {
 				router.push("/auth");
@@ -35,8 +41,25 @@ const AppWrapper = ({ children }: { children: React.ReactNode }) => {
 		initializeData();
 	}, [dispatch, router]);
 
-	useSyncOnPageUnload(modifiedUsers);
-	useSyncOnRouteChange(modifiedUsers);
+	// Define sync functions
+	const syncModifiedUsers = (data: User[]) => {
+		console.log("Syncing modified users on route change", data);
+	};
+
+	const syncModifiedEmployees = (data: Employee[]) => {
+		console.log("Syncing modified employees on route change", data);
+	};
+
+	// Handle unload and route change
+	useSyncOnPageUnload("users", modifiedUsers, syncModifiedUsers);
+	useSyncOnPageUnload("employees", modifiedEmployees, syncModifiedEmployees);
+
+	useSyncOnRouteChange(modifiedUsers, "/dashboard/users", syncModifiedUsers);
+	useSyncOnRouteChange(
+		modifiedEmployees,
+		"/dashboard/employees",
+		syncModifiedEmployees
+	);
 
 	return <>{children}</>;
 };
